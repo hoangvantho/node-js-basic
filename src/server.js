0c6aa200ea09d6925b8bacad3a6c3d16d6af4aa6
Tho Hoang <hoangvantho@gmail.com>
2023-02-13 12:44:34 +0700

    video6

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+import configViewEngine from './configs/viewEngine';
+const app = express();
+// const path = require('path');
 const port = 3000
 
+configViewEngine(app);
+
 app.get('/', (req, res) => {
-  res.send('Hello Thang theo!')
+    res.render('index.ejs');
 })
 app.get('/about',(req,res)=>{
     res.send('I am Hoang Van THo')
