2274ef8ffb163f23e028e5c59433a88fd252e4ab
Tho Hoang <hoangvantho@gmail.com>
2023-02-20 14:56:50 +0700

    API

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 import configViewEngine from './configs/viewEngine';
 import initWebRoute from './route/web';
+import initAPIRoute from './route/api';
 // import connection from './configs/connectDB';
 
 require('dotenv').config();
@@ -17,6 +18,10 @@ configViewEngine(app);
 
 // init web route
 initWebRoute(app);
+
+//init API route
+initAPIRoute(app);
+
 
 
 app.listen(port, () => {
